58d7bea97f22cdabb82796ae869558abd2fcd70c
V6NV5337 <lso7182@naver.com>
2026-07-20 13:59:19 +0900

    init: 프로젝트를 초기화합니다. [SEAS-1]

---
+++ src/main/java/com/cmcni/sales_erp_api_server/SalesErpApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.cmcni.sales_erp_api_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesErpApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesErpApiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmcni/sales_erp_api_server/SalesErpApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmcni.sales_erp_api_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesErpApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
