8811d9507eb39cdb565a9870d09b50aaa6f46c9e
V6NV5337 <lso7182@naver.com>
2026-07-23 14:46:43 +0900

    feat: [이상아] CORS 정책을 정의하기 위해 작성하는 설정 클래스를 구현합니다. [SMSB-8]

---
+++ src/main/java/com/cmcni/sales_management_system_backend/common/corsconfig/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.cmcni.sales_management_system_backend.common.corsconfig;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${allowed_origins}")
+    private String[] allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+    
+}
